cb425abdfaf1d603467eae5e6f2cb6987d510799
Wenguang Zhou <59040826+Joesiphers@users.noreply.github.com>
2024-06-18 07:10:34 +0000

    add google github login

--- app/pages/contact/page.tsx
+++ app/pages/contact/page.tsx
@@ -1,22 +1,27 @@
-import { signInS } from "./signin";
+import { signInGithub, signInGoogle } from "./signin";
 /*        <form action={signInS ("github")}>
           <button type="sumit">GitHub</button>
         </form>
         */
-export default async  function Tech() {
+export default async function Tech() {
   return (
     <div className="">
       <div>
         <p className="text-4xl m-2">
           contact GuangZhou WanWei email : joe@wwin.cn
         </p>
-      ?</div>
+        ?
+      </div>
       <div>
-        <p className="text-4xl m-2">
-          Google
-        </p>
-        <form action={signInS}>
+        <p className="text-4xl m-2">Google</p>
+        <form action={signInGoogle}>
           <button type="sumit">Google</button>
+        </form>
+      </div>
+      <div>
+        <p className="text-4xl m-2">Github</p>
+        <form action={signInGithub}>
+          <button type="sumit">Github</button>
         </form>
       </div>
     </div>
--- app/pages/contact/signin.ts
+++ app/pages/contact/signin.ts
@@ -1,13 +1,16 @@
 "use server";
 import { signIn, signOut } from "@/auth";
 
-export async function signInS() {
-       await signIn("google", {
-          callbackUrl: "http://localhost:3000",
-        });
-      };
-    
-
+export async function signInGithub() {
+  await signIn("github", {
+    callbackUrl: "https://3jkjdm-3000.csb.app",
+  });
+}
+export async function signInGoogle() {
+  await signIn("google", {
+    callbackUrl: "https://3jkjdm-3000.csb.app",
+  });
+}
 export async function signOutS() {
   return await signOut();
 }
